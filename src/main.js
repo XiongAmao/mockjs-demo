4d7388cfdd61421ca0d8cfe105d50207558109f0
XiongAmao <onycat@live.cn>
2017-12-19 14:56:17 +0800

    Add: fake data & intercept axios request

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Axios from 'axios'
+import mockdata from '../mock/mockdata.js' // 跑数据了
 
+Vue.prototype.$axios = Axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
